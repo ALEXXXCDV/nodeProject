56f308ced07d7763087e48d6cf3f090de53474ad
Alejandro Cadavid <alecad76.ac@gmail.com>
2024-08-26 21:53:36 -0500

    Add get categories by id function and endpoint

--- src/controllers/categories_controller.ts
+++ src/controllers/categories_controller.ts
@@ -2,7 +2,12 @@ import { QueryResult } from "pg";
 import pool from "../database/db_connect";
 import { Request, Response } from "express";
 
-
+/**
+ * Get all data of categories table.
+ * @param req 
+ * @param res 
+ * @returns Categories
+ */
 export const getCategories = async (req: Request, res: Response): Promise<Response> =>{
     try{
         const response: QueryResult = await  pool.query('SELECT * FROM categories;');
@@ -12,3 +17,20 @@ export const getCategories = async (req: Request, res: Response): Promise<Respon
         return res.status(500).json('Internal Server Eror');
     }
 };
+
+/**
+ * Get all data of categories table by id
+ * @param req 
+ * @param res 
+ * @returns Categories by ID
+ */
+export const getCategoriesById = async (req:Request, res:Response): Promise<Response> => {
+    const id = parseInt(req.params.id);
+    try {
+        const response: QueryResult = await pool.query('SELECT * FROM categories WHERE category_id = $1', [id]);
+        return res.json(response.rows);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json('Internal Server Eror');
+    }
+}
